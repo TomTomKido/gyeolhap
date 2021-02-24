296a811574fe03bc8fd852d81978d18876e10f77
taelee <taelee@student.42seoul.kr>
2021-02-24 16:56:59 +0900

    GameScreen수정: - 컬렉션뷰를 2개 만들었지만 셀들이 짤려서 나오는 문제 발생

---
+++ GyoelHap/GameScreen/AnswerCell.swift
@@ -0,0 +1,13 @@
+//
+//  AnswerCell.swift
+//  GyeolHap
+//
+//  Created by Terry Lee on 2/23/21.
+//
+
+import UIKit
+
+class AnswerCell: UICollectionViewCell {
+    @IBOutlet weak var label: UILabel!
+    
+}
--- GyoelHap/GameScreen/PlayViewController.swift
+++ GyoelHap/GameScreen/PlayViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 class PlayViewController: UIViewController {
 //    let stageManager = StageManager.shared
 
-    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var collectionViewUp: UICollectionView!
+    @IBOutlet weak var collectionViewDown: UICollectionView!
     
     var currentStage: Stage?
     var currentStageManager: CurrentStageManager?
@@ -19,61 +20,70 @@ class PlayViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
 //        self.navigationController?.isNavigationBarHidden = true
-        collectionView.delegate = self
-        collectionView.dataSource = self
+        collectionViewUp.delegate = self
+        collectionViewUp.dataSource = self
         
+        collectionViewDown.delegate = self
+        collectionViewDown.dataSource = self
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         guard let item = currentStage else { return }
         self.currentStageManager = CurrentStageManager(stage: item)
-        
         print("정답리스트: \((currentStageManager?.answers)!)")
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 }
 
 extension PlayViewController: UICollectionViewDataSource {
 
+    //셀을 몇개의 섹션(컬럼)으로 보여줄까?
+//    func numberOfSections(in collectionView: UICollectionView) -> Int {
+//        if collectionView == collectionViewUp {
+//            return 3
+//        } else {
+//            return 2
+//        }
+//    }
     //셀을 몇개 보여줄까?
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 9
+        if collectionView == self.collectionViewUp {
+            return 9
+        } else {
+            return 12
+        }
     }
     
     //컬렉션 뷰 셀 어떻게 보여줄까?
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TileCollectionViewCell", for: indexPath) as? TileCollectionViewCell else {
-            return UICollectionViewCell()
+        if collectionView == collectionViewUp {
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TileCollectionViewCell", for: indexPath) as? TileCollectionViewCell else {
+                return UICollectionViewCell()
+            }
+            cell.updateUI(index: indexPath.item, item: currentStage?.dataArray[indexPath.item] ?? 0)
+            cell.tapHandler = {
+                guard let manager = self.currentStageManager else { return }
+                manager.addToTryList(indexPath.item + 1)
+                manager.printTryList()
+            }
+            cell.isClicked = {
+                //TODO: manager 옵셔널 바인딩을 실패했을 때 return false부분이 맞는지 알아보고 바꾸기
+                guard let manager = self.currentStageManager else { return [] }
+                return manager.tryList
+            }
+            //리로드하면 랜덤하게 색칠됨
+            cell.reloadView = {
+                self.collectionViewUp.reloadData()
+            }
+            return cell
+        } else {
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "AnswerCell", for: indexPath) as? AnswerCell else {
+                return UICollectionViewCell()
+            }
+            cell.backgroundColor = .black
+            return cell
         }
-        cell.updateUI(index: indexPath.item, item: currentStage?.dataArray[indexPath.item] ?? 0)
-        cell.tapHandler = {
-            guard let manager = self.currentStageManager else { return }
-            manager.addToTryList(indexPath.item + 1)
-            manager.printTryList()
-        }
-        cell.isClicked = {
-            //TODO: manager 옵셔널 바인딩을 실패했을 때 return false부분이 맞는지 알아보고 바꾸기
-            guard let manager = self.currentStageManager else { return [] }
-            return manager.tryList
-        }
-        //리로드하면 랜덤하게 색칠됨
-        cell.reloadView = {
-            self.collectionView.reloadData()            
-        }
-        return cell
     }
-
 }
 
 
@@ -87,9 +97,18 @@ extension PlayViewController: UICollectionViewDelegate {
 extension PlayViewController:UICollectionViewDelegateFlowLayout {
     //셀 사이즈 어떻게 할까?
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let inset: CGFloat = 20
-        let width: CGFloat = (collectionView.bounds.width - inset * 4) / 3
-        let height: CGFloat = width + 30
-        return CGSize(width:width, height: height)
+        if collectionView == collectionViewUp {
+            let inset: CGFloat = 20
+            let width: CGFloat = (collectionView.bounds.width - inset * 4) / 3
+            let height: CGFloat = (collectionView.bounds.width - inset * 4) / 3
+            return CGSize(width:width, height: height)
+        } else {
+            let inset: CGFloat = 5
+            let width: CGFloat = (collectionView.bounds.width - inset * 3) / 2
+            let height: CGFloat = collectionView.bounds.width / 6
+//            let width: CGFloat = 80
+//            let height: CGFloat = 60
+            return CGSize(width:width, height: height)
+        }
     }
 }
