c583a0b0df5a1b8f9d6b99f83240b293fb6a10e7
taelee <taelee@student.42seoul.kr>
2021-03-10 20:32:32 +0900

    MainScreen: - 메인 화면 추가 - play, how to play, exit 버튼 추가 - play 버튼과 exit버튼은 구현 완료 - play 버튼 클릭시 StageScreen으로 가는데 네비게이션 바가 사라져서 새로 만들어줘야하는 상황

---
+++ GyoelHap/MainScreen/MainViewController.swift
@@ -0,0 +1,43 @@
+//
+//  MainViewController.swift
+//  GyeolHap
+//
+//  Created by TaeHyeok LEE on 2021/03/10.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var howToPlayButton: UIButton!
+    @IBOutlet weak var EXITButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func goToStageScreen(_ sender: UIButton) {
+        let stageStoryboard = UIStoryboard.init(name: "Stage", bundle: nil)
+        guard let stageVC = stageStoryboard.instantiateViewController(identifier: "StageVC") as? StageTableViewController else { return }
+        self.navigationController?.pushViewController(stageVC, animated: true)
+    }
+    @IBAction func goToHowToScreen(_ sender: UIButton) {
+    }
+    
+    @IBAction func EXIT(_ sender: UIButton) {
+        exit(0)
+    }
+}
