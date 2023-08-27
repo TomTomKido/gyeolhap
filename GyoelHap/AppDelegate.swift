a2dcd71e19ecf926751744a5534754fdd13cfecf
terry.yes <terry.yes@kakaoent.com>
2023-08-28 00:19:11 +0900

    [*] 메인뷰컨트롤러에 광고 추가 성공

--- GyoelHap/AppDelegate.swift
+++ GyoelHap/AppDelegate.swift
@@ -7,15 +7,15 @@
 
 import UIKit
 import RealmSwift
+import GoogleMobileAds
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         initializeRealm()
+        GADMobileAds.sharedInstance().start()
         return true
     }
 
