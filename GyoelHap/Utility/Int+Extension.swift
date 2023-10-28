a6214d95c9a4249fd192fb71c4e1ccecf8410008
Joey <isjoeyful@gmail.com>
2023-10-28 19:17:49 +0900

    timeString extension 구현

---
+++ GyoelHap/Utility/Int+Extension.swift
@@ -0,0 +1,24 @@
+//
+//  Int+Extension.swift
+//  GyeolHap
+//
+//  Created by JeongAh Hong on 2023/10/28.
+//
+
+import Foundation
+
+extension Int {
+    func timeString() -> String {
+        return TimeInterval(self).timeString()
+    }
+}
+
+extension Double {
+    func timeString() -> String {
+        let newTime = self / 10
+        let hours = Int(newTime) / 3600
+        let minutes = Int(newTime) / 60 % 60
+        let seconds = Int(newTime) % 60
+        return String(format:"%02i:%02i:%02i", hours, minutes, seconds)
+    }
+}
