1c9cd6d9bef20c4b60410dddcb2b80e081ec9eb8
Joey <isjoeyful@gmail.com>
2023-10-30 20:49:17 +0900

    Color extension 추가

---
+++ GyoelHap/Utility/Color+Extension.swift
@@ -0,0 +1,15 @@
+//
+//  Color+Extension.swift
+//  GyeolHap
+//
+//  Created by JeongAh Hong on 2023/10/29.
+//
+
+import UIKit
+
+extension UIColor {
+    static let correctOColor = UIColor(named: "correct_o_image")
+    static let wrongXColor = UIColor(named: "wrong_x_image")
+    
+    static let hotpink = UIColor(named: "hotpink1")
+}
